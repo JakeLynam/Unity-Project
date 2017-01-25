e010f00af28ae470323861b258e695b064586cd4
Robert Jacob Lynam <rjacoblynam@gmail.com>
2017-01-25 11:16:59 -0700

    Updated Cube with red and blue

---
+++ Unity Project/Assets/changeColor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeColor : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            print("Changed color to red");
+
+        }
+        // turns object to BLUE
+
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+            print("Changed color to Blue");
+        }
+
+    }
+}
